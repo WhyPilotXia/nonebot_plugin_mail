9414b4a59850004392270b233ecf4f85eb9ca959
WhyPilotXia <2743218818@qq.com>
2026-06-16 18:58:00 +0800

    修复nonebot_plugin_mail/constants.py 里提前普通导入了 nonebot_plugin_localstore，导致测试后面再 require(nonebot_plugin_localstore) 时，NoneBot 发现模块已导入但不是插件加载状态，于是报 Module nonebot_plugin_localstore is not loaded as a plugin。

--- build/lib/nonebot_plugin_mail/data/__init__.py
+++
@@ -1,4 +0,0 @@
-﻿# python3
-# -*- coding: utf-8 -*-
-# @Version : 0.7.0
-# 规划备注：叶恩杰项目 FALLBACK_CONTACTS 转 Python
--- build/lib/nonebot_plugin_mail/services/__init__.py
+++
@@ -1,4 +0,0 @@
-﻿# python3
-# -*- coding: utf-8 -*-
-# @Version : 0.7.0
-# 规划备注：业务服务模块包
